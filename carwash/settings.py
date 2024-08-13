da0f57a1a598ce95849b8e6b4749cbed15b0a6f1
unknown <andy.luarte@gmail.com>
2024-08-13 18:01:12 -0400

    cambio ingreso.html y forms.py21

--- carwash/settings.py
+++ carwash/settings.py
@@ -133,7 +133,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
+
 TIME_ZONE = 'America/Santiago'
 
 USE_I18N = True
