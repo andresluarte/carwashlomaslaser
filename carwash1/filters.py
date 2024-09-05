0f63572d3023dfebd40e38433bc914da81d879ee
unknown <andy.luarte@gmail.com>
2024-09-04 21:03:23 -0400

    filter fecha

--- carwash1/filters.py
+++ carwash1/filters.py
@@ -11,6 +11,10 @@ class FilterIngreso(django_filters.FilterSet):
     lavado = django_filters.ChoiceFilter(label='Lavado', choices=Ingreso.LAVADO_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
     vehiculo = django_filters.ChoiceFilter(label='Vehículo', choices=Ingreso.VEHICULO_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
     estado_vehiculo = django_filters.ChoiceFilter(label='Estado Vehiculo', choices=Ingreso.ESTADO_VEHICULO_CHOICES, widget=forms.Select(attrs={'class': 'form-control'}))
+    fecha_ingreso = django_filters.DateFromToRangeFilter(
+        label='Fecha de Ingreso',
+        widget=django_filters.widgets.RangeWidget(attrs={'class': 'form-control', 'type': 'date'})
+    )
     class Meta:
         model = Ingreso
         fields = ['patente', 'tipo_de_pago', 'tipo_doc', 'lavado', 'vehiculo','estado_vehiculo']
